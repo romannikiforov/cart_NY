e917d163388323d80f2cc44256f727371d83889e
romannikiforov <escaro@ukr.net>
2024-11-26 17:44:27 +0200

    add opencart

--- js/opencart.js
+++ js/opencart.js
@@ -4,7 +4,10 @@ function initOpencart() {
   tlOpen
     .from("#wrap-opencart", { ease: "linear", autoAlpha: 0 })
     .from("#back-left", { scaleX: 0, duration: 1 })
-    .from("#back-right", { scaleX: 0, duration: 1 }, "<");
+    .from("#back-right", { scaleX: 0, duration: 1 }, "<")
+    .from(".sign_bottom > path", { stagger: 0.04, duration: 0.3 })
+    .from("#orbix_textil_top > *", { stagger: 0.04, duration: 0.4 })
+    .from("#orbix-textil_bottom > *", { stagger: 0.04, duration: 0.4 });
 }
 
 window.addEventListener("load", function () {
